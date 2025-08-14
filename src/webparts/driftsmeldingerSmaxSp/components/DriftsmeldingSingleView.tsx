d2dc9ef3597d45c420d0af49e44e01200c2ed165
Jose Luis Suarez Cabrera <jose.luis.suarez@puzzlepart.com>
2025-08-14 12:54:29 +0200

    Removing usestate that is not used

--- src/webparts/driftsmeldingerSmaxSp/components/DriftsmeldingSingleView.tsx
+++ src/webparts/driftsmeldingerSmaxSp/components/DriftsmeldingSingleView.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
 import { useDriftsmeldinger } from '../hooks/useDriftsmeldinger';
 import { Spinner, DefaultButton, PrimaryButton } from '@fluentui/react';
 import styles from './DriftsmeldingerSmaxSp.module.scss';
